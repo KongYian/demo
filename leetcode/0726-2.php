bd82dd49d8d2ee2f0ac571bba1ae3cbadededb1d
KongYian <937069176@qq.com>
2019-07-26 18:21:01 +0800

    2019-07-26 18-21-01 autocommit

--- leetcode/0726-2.php
+++ leetcode/0726-2.php
@@ -15,6 +15,10 @@ X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
 C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
 
  */
+
+$num = 320;
+echo $num % 100;
+exit;
 $num = '96';
 echo intToRoman($num);
 
@@ -72,8 +76,8 @@ function intToRoman($num) {
             return $arr[$num];
         } else {
             $first = $num % 100 ;
-            $second = $num - $first;
-            
+            $second = ($num - $first) % 100;
+
 
         }
     }
