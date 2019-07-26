f3044694784059d573bee18e4fe2bb275a5e7fec
KongYian <937069176@qq.com>
2019-07-26 18:19:01 +0800

    2019-07-26 18-19-01 autocommit

--- leetcode/0726-2.php
+++ leetcode/0726-2.php
@@ -33,12 +33,22 @@ function intToRoman($num) {
         8 => 'VIII',
         9 => 'IX',
         10 => 'X',
+        20 => 'XX',
+        30 => 'XXX',
         40 => 'XL',
         50 => 'L',
+        60 => 'LX',
+        70 => 'LXX',
+        80 => 'LXX',
         90 => 'XC',
         100 => 'C',
+        200 => 'CC',
+        300 => 'CCC',
         400 => 'CD',
         500 => 'D',
+        600 => 'DD',
+        700 => 'DDD',
+        800 => 'DDD',
         900 => 'CM',
         1000 => 'M',
     ];
@@ -52,11 +62,19 @@ function intToRoman($num) {
         } else {
             $first = $num % 10 ;
             $second = $num - $first;
-            echo $first.PHP_EOL;
-            echo $second.PHP_EOL;
             if(array_key_exists($second,$arr)) {
                 return $arr[$second] . $arr[$first];
             }
+        }
+    }
+    if($num <= 1000) {
+        if(array_key_exists($num,$arr)) {
+            return $arr[$num];
+        } else {
+            $first = $num % 100 ;
+            $second = $num - $first;
+            
+
         }
     }
 }
