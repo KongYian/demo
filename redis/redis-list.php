86a7ca3c659cc4043790f6e0c0cce5686cff4d45
KongYian <937069176@qq.com>
2019-08-06 18:00:03 +0800

    2019-08-06 18-00-01 autocommit

--- redis/redis-list.php
+++ redis/redis-list.php
@@ -1,6 +1,7 @@
 <?php
 $rs = new Redis();
 $rs->connect('127.0.0.1');
+//$rs->flushAll();
 $key = 'list-key';
 //echo $rs->rPush($key,1) . PHP_EOL;
 //echo $rs->rPush($key,2) . PHP_EOL;
@@ -10,7 +11,16 @@ $key = 'list-key';
 //var_dump($rs->lRange($key,0,-1)) . PHP_EOL;
 //$rs->del($key);
 //echo $rs->lPush($key,2);
-//echo $rs->lPush($key,22);
-echo $rs->rPush($key,2);
-echo $rs->rPush($key,22);
+//echo $rs->lPush($key,3);
+//echo $rs->rPush($key,4);
+//echo $rs->rPush($key,5);
+//echo $rs->rPush($key,6);
+//echo $rs->rPush($key,7);
+//echo $rs->rPush($key,22);
+//echo $rs->rPush($key,22);
+//var_dump($rs->lRange($key,0,-1));
+
+$rs->lTrim($key,0,2);
 var_dump($rs->lRange($key,0,-1));
+
+$rs->brpoplpush();
--- redis/redis-string.php
+++ redis/redis-string.php
@@ -2,6 +2,18 @@
 $rs = new Redis();
 $rs->connect('127.0.0.1');
 $key = 'string-key';
-echo $rs->set($key,123).PHP_EOL;
-echo $rs->get($key).PHP_EOL;
-echo $rs->del($key).PHP_EOL;
+//echo $rs->set($key,123).PHP_EOL;
+//echo $rs->get($key).PHP_EOL;
+//echo $rs->del($key).PHP_EOL;
+//echo $rs->incr($key);
+//echo $rs->set($key,'helloworld').PHP_EOL;
+//echo $rs->getRange($key,0,4).PHP_EOL;
+//echo $rs->setRange($key,5,'taotao').PHP_EOL;
+//echo $rs->get($key) .PHP_EOL;
+
+//echo $rs->append('hi','xxxxxx') . PHP_EOL;
+
+ echo $rs->set('key', "*"). PHP_EOL;     // ord("*") = 42 = 0x2f = "0010 1010"
+ echo $rs->setBit('key', 5, 1). PHP_EOL; // returns 0
+ echo $rs->setBit('key', 7, 1). PHP_EOL; // returns 0
+ echo $rs->get('key'). PHP_EOL;          // chr(0x2f) = "/" = b("0010 1111")
