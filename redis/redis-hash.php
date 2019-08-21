aad05f78fb22475804c675bef3ae38f803de6684
KongYian <937069176@qq.com>
2019-08-21 18:00:01 +0800

    2019-08-21 18-00-01 autocommit

--- redis/redis-hash.php
+++ redis/redis-hash.php
@@ -2,10 +2,11 @@
 $rs = new Redis();
 $rs->connect('127.0.0.1');
 $key = 'hash-key';
+$rs->del($key);
 $rs->hSet($key,'name','tao');
-$rs->hSet($key,'sex','male');
-$rs->hSet($key,'age',18);
-var_dump($rs->hGet($key,'name'));
+//$rs->hSet($key,'sex','male');
+//$rs->hSet($key,'age',18);
+//var_dump($rs->hGet($key,'name'));
 var_dump($rs->hGetAll($key));
-var_dump($rs->hDel($key,'name'));
-var_dump($rs->hGetAll($key));
+//var_dump($rs->hDel($key,'name'));
+//var_dump($rs->hGetAll($key));
---
+++ redis/redis-trans.php
@@ -0,0 +1,3 @@
+<?php
+$rs = new Redis();
+$rs->connect('127.0.0.1');
